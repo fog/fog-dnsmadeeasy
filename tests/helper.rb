ceb493660c9da8d483deeb5097ff1591384999bd
Ricardo Aravena <raravena@branch.io>
2017-10-17 12:52:35 -0700

    Fix for code climate

--- tests/helper.rb
+++ tests/helper.rb
@@ -1,6 +1,5 @@
 begin
-  require 'codeclimate-test-reporter'
-  CodeClimate::TestReporter.start
+  require 'SimpleCov'
 rescue LoadError => e
   $stderr.puts "not recording test coverage: #{e.inspect}"
 end
